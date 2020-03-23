447ab6954721cba2ab775128d0971c6b3d5606c1
Daniel Gilbert <ich@danielgilbert.de>
2020-03-23 17:56:25 +0100

    Adds Windows App

---
+++ dgMicMute.CoreApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dgMicMute.CoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
